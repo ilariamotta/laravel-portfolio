5f4f451e16f498f0c847f51cdeb42be919d71215
ilariamotta <ilaria.motta93@gmail.com>
2026-04-08 11:53:01 +0200

    feat: route collegata a middleware per amministrazione

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,13 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+// MIE ROUTES
+
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/index', [DashboardController::class, 'index'])->name('index');
+    });
