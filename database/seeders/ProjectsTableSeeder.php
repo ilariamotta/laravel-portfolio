9e6f4728977a62cace0305f603b075b2e4eefe46
ilariamotta <ilaria.motta93@gmail.com>
2026-04-09 17:14:01 +0200

    feat: migration, project table, seeader create

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+
+
+// importazione faker 
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+    }
+}
