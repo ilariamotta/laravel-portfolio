f39a8b43a97402222d8e914fca6a7b4ea6091ea6
ilariamotta <ilaria.motta93@gmail.com>
2026-04-09 18:16:28 +0200

    feat: seeder con dati creato

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -10,14 +10,24 @@ use App\Models\Project;
 
 
 // importazione faker 
+use Faker\Generator as Faker;
 
 class ProjectsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        
+        for($i = 0; $i < 10; $i++) {
+                        $newproject = new Project();
+
+            $newproject->name = $faker->sentence(3);
+            $newproject->customer = $faker->company();
+            $newproject->time = $faker->date('Y-m-d');
+            $newproject->description = $faker->paragraph();
+
+            $newproject->save();
+        }
     }
 }
