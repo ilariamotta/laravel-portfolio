f80fd6b3e8c6be62164b2249c5248e7cf4dacda3
ilariamotta <ilaria.motta93@gmail.com>
2026-04-14 11:21:28 +0200

    fix: styling index

--- resources/views/backoffice/index.blade.php
+++ resources/views/backoffice/index.blade.php
@@ -1,41 +1,53 @@
 @extends('backoffice.layouts.adminmaster')
 
 @section('dashboard')
-    <main class=" py-4 min-vh-80">
-        <h2 class="text-black-warm">Benvenuto, {{ $user->name }}!</h2>
-    <div class="text-black-warm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo maxime quo repellat esse molestias? Pariatur hic accusamus, reprehenderit ullam minima voluptates magni nobis consectetur, iure est, cum aliquid minus voluptatem.</div>
+<main class="py-4 min-vh-80">
+    <div class="container">
+        <h2 class="text-black-warm mb-3">Benvenuto, {{ $user->name }}!</h2>
 
-<h3 class="py-3">Visualizza il tuo profilo</h3>
-<a href="{{ route('admin.profile') }}" class="bg-lilac text-butter py-2 px-4 rounded-pill text-decoration-none">Vai al tuo profilo</a>
-<h3 class="py-3">Lista progetti</h3>
-      <div class="table-responsive mb-5 rounded-4 shadow-sm overflow-hidden custom-table-wrapper">
-            <table class="table table-hover align-middle mb-0 custom-project-table">
-                <thead class="bg-black-warm text-lilac">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Cliente</th>
-                        <th>Periodo</th>
-                        <th class="text-end">Azioni</th>
-                    </tr>
-                </thead>
+        <div class="text-black-warm mb-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo maxime quo repellat esse molestias? Pariatur hic accusamus, reprehenderit ullam minima voluptates magni nobis consectetur, iure est, cum aliquid minus voluptatem.
+        </div>
 
-                <tbody>
-                    @foreach ($projects as $project)
+        <h3 class="py-3">Visualizza il tuo profilo</h3>
+        <a href="{{ route('admin.profile') }}" class="bg-lilac text-butter py-2 px-4 rounded-pill text-decoration-none d-inline-block mb-4">
+            Vai al tuo profilo
+        </a>
+
+        <h3 class="py-3 text-black-warm">Lista progetti</h3>
+
+        <div class="project-show-card p-3 p-md-4 rounded-4 shadow-sm">
+            <div class="table-responsive centered-table">
+                <table class="table align-middle mb-0 custom-project-table">
+                    <thead>
                         <tr>
-                            <td>{{ $project->id }}</td>
-                            <td class="fw-bold text-pink">{{ $project->name }}</td>
-                            <td>{{ $project->customer }}</td>
-                            <td>{{ $project->time }}</td>
-                            <td class="text-end">
-                                <a href="{{ route('admin.show', $project) }}"
-                                   class="btn btn-sm btn-project-show">
-                                    Vedi
-                                </a>
-                            </td>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Cliente</th>
+                            <th>Periodo</th>
+                            <th class="text-end">Azioni</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <td>{{ $project->id }}</td>
+                                <td class="fw-bold text-pink">{{ $project->name }}</td>
+                                <td>{{ $project->customer }}</td>
+                                <td>{{ $project->time }}</td>
+                                <td class="text-end">
+                                    <a href="{{ route('admin.show', $project) }}"
+                                       class="btn btn-sm btn-project-show">
+                                        Vedi
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -35,3 +35,4 @@ Route::middleware(['auth', 'verified'])
     });
 
 
+Route::resource("projects", ProjectController::class)->middleware(["auth", "verified"]);
