4bde02c74b3e9f59b7915e635c13f0c9aafc53d1
ilariamotta <ilaria.motta93@gmail.com>
2026-04-17 16:28:46 +0200

    feat:  destroy funzionte

--- resources/views/backoffice/projects/index.blade.php
+++ resources/views/backoffice/projects/index.blade.php
@@ -3,15 +3,20 @@
 @section('dashboard')
 <main class="py-4 min-vh-80">
     <div class="container">
-        <h2 class="text-black-warm mb-3">Benvenuto,  {{ auth()->user()->name }}!</h2>
+        <h2 class="text-black-warm mb-3">Benvenuto, {{ auth()->user()->name }}!</h2>
 
         <div class="text-black-warm mb-4">
-            Da questa sezione puoi gestire i progetti del tuo portfolio: visualizzare i dettagli, modificare le informazioni o eliminarli. Clicca su "Vedi" per accedere alla pagina di gestione di ciascun progetto oppure clicca sul pulsante "Aggiungi nuovo progetto".
-            <br> Altrimenti gestisci le categorie dei progetti
-            <a href="{{ route("admin.types.index") }}" class="text-pink">qui</a>.
+            Da questa sezione puoi gestire i progetti del tuo portfolio: visualizzare i dettagli, modificare le informazioni o eliminarli. 
+            Clicca su "Vedi" per accedere alla pagina di gestione di ciascun progetto oppure clicca sul pulsante "Aggiungi nuovo progetto".
+            <br>
+            Altrimenti gestisci le categorie dei progetti
+            <a href="{{ route('admin.types.index') }}" class="text-pink">qui</a>.
         </div>
 
-  <a href="{{ route('admin.projects.create') }}" class="btn btn-sm btn-project-create my-2">Aggiungi nuovo progetto</a>
+        <a href="{{ route('admin.projects.create') }}" class="btn btn-sm btn-project-create my-2">
+            Aggiungi nuovo progetto
+        </a>
+
         <div class="p-3 p-md-4 rounded-4 shadow-sm">
             <div class="table-responsive centered-table">
                 <table class="table align-middle mb-0 custom-project-table">
@@ -33,11 +38,19 @@
                                 <td class="fw-bold text-pink">{{ $project->name }}</td>
                                 <td>{{ $project->customer }}</td>
                                 <td>{{ $project->time }}</td>
-                                <td>{{ $project->type->name }}</td>
+                                <td>{{ $project->type?->name }}</td>
                                 <td class="text-end">
                                     <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-sm btn-project-show">Vedi</a>
                                     <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-sm btn-project-show">Modifica</a>
-                                    <button type="button" class="btn btn-project-delete btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">Elimina</button>
+
+                                    <button
+                                        type="button"
+                                        class="btn btn-project-delete btn-sm"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#deleteProjectModal{{ $project->id }}"
+                                    >
+                                        Elimina
+                                    </button>
                                 </td>
                             </tr>
                         @endforeach
@@ -47,29 +60,33 @@
         </div>
     </div>
 </main>
-@endsection
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">ATTENZIONE</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Vuoi davvero eliminare il progetto <strong>{{ $project->name }}</strong>?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-outline-dark rounded-pill px-4" data-bs-dismiss="modal">Annulla</button>
-        
-        {{-- elimina --}}
-        <form action="{{ route("admin.projects.destroy", $project) }}" method="POST">
-   @csrf
-   @method("DELETE")
-   <input type="submit" value="Elimina definitivamente" class="btn-project-delete btn btn-sm">
-</form>
-      </div>
+@foreach ($projects as $project)
+    <div class="modal fade" id="deleteProjectModal{{ $project->id }}" tabindex="-1" aria-labelledby="deleteProjectModalLabel{{ $project->id }}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteProjectModalLabel{{ $project->id }}">ATTENZIONE</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    Vuoi davvero eliminare il progetto <strong>{{ $project->name }}</strong>?
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-dark rounded-pill px-4" data-bs-dismiss="modal">
+                        Annulla
+                    </button>
+
+                    <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Elimina definitivamente" class="btn-project-delete btn btn-sm">
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
+@endforeach
+@endsection
--- resources/views/backoffice/types/index.blade.php
+++ resources/views/backoffice/types/index.blade.php
@@ -3,33 +3,78 @@
 @section('dashboard')
 <main class="py-4 min-vh-80">
     <div class="d-flex justify-content-center align-items-center">
-    <a href="{{ route('admin.types.create') }}" class="btn btn-sm btn-project-create my-2">Aggiungi nuova categoria progetto</a>
+        <a href="{{ route('admin.types.create') }}" class="btn btn-sm btn-project-create mb-4">
+            Aggiungi nuova categoria progetto
+        </a>
     </div>
-    <div class="d-flex justify-content-center">
-            <div class="table-responsive centered-table">
-                <table class="table align-middle mb-0 custom-type-table">
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th class="text-end">Azioni</th>
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        @foreach ($types as $type)
-                            <tr>
-                                <td class="me-5 pe-5">{{ $type->name }}</td>
-                                <td class="text-end">
-                                    
-                                    <a href="{{ route('admin.types.edit', $type) }}" class="btn btn-sm btn-project-show">Modifica</a>
-                                    <button type="button" class="btn btn-project-delete btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">Elimina</button>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    <div class="d-flex justify-content-center">
+        <div class="table-responsive centered-table">
+            <table class="table align-middle mb-0 custom-type-table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th class="text-end">Azioni</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($types as $type)
+                        <tr>
+                            <td class="me-5 pe-5">{{ $type->name }}</td>
+                            <td class="text-end">
+                                <a href="{{ route('admin.types.edit', $type) }}" class="btn btn-sm btn-project-show">
+                                    Modifica
+                                </a>
+
+                                <button
+                                    type="button"
+                                    class="btn btn-project-delete btn-sm"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal{{ $type->id }}">
+                                    Elimina
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center mt-4">
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-project-show px-4 py-2">
+            ← Torna ai progetti
+        </a>
+    </div>
+</main>
+
+@foreach ($types as $type)
+    <div class="modal fade" id="exampleModal{{ $type->id }}" tabindex="-1" aria-labelledby="exampleModalLabel{{ $type->id }}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel{{ $type->id }}">ATTENZIONE</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    Vuoi davvero eliminare la categoria <strong>{{ $type->name }}</strong>?
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-dark rounded-pill px-4" data-bs-dismiss="modal">
+                        Annulla
+                    </button>
+
+                    <form action="{{ route('admin.types.destroy', $type) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Elimina definitivamente" class="btn-project-delete btn btn-sm">
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</main>
+@endforeach
 @endsection
