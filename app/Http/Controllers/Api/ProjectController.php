06b380894b96f0a8368716f82fd5e7975dd7eb28
ilariamotta <ilaria.motta93@gmail.com>
2026-04-22 15:45:04 +0200

    feat: creato endpoint index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index() {
-        return "sei nella index api";
+        $projects = Project::all();
+        return response()->json([
+            "success"=> true,
+            "data"=>$projects
+        ]);
     }
 }
